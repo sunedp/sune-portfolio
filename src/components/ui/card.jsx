15145a88b1d135fce37059578b4e381798b928e8
sunedp <dupreezs@gmail.com>
2025-04-25 10:39:43 +0200

    Delete src directory

--- src/components/ui/card.jsx
+++
@@ -1,7 +0,0 @@
-export function Card({ children }) {
-  return <div className="rounded-xl border p-4 bg-white">{children}</div>;
-}
-
-export function CardContent({ children }) {
-  return <div>{children}</div>;
-}
